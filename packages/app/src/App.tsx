a1cdccee1ba7547121150dba2676e45e6a03a7d4
Prashanth Sreepathi <s.prasantreddy94@gmail.com>
2026-04-28 23:53:51 -0500

    techdocs: add mermaid support to techdocs

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -2,6 +2,9 @@ import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
 import { navModule } from './modules/nav';
 
+import techDocsPlugin from '@backstage/plugin-techdocs/alpha';
+import { techDocsMermaidAddonModule } from "backstage-plugin-techdocs-addon-mermaid";
+
 import { githubAuthApiRef } from '@backstage/core-plugin-api';
 import { SignInPageBlueprint } from '@backstage/plugin-app-react';
 import { SignInPage } from '@backstage/core-components';
@@ -29,6 +32,8 @@ export default createApp({
   features: [
     catalogPlugin, 
     navModule,
+    techDocsPlugin,
+    techDocsMermaidAddonModule,
     createFrontendModule({
       pluginId: 'app',
       extensions: [signInPage],
